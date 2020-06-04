57eece0f58470a35828f04f1280ed32fb5185331
Gazinur <halitulla@gmail.com>
2020-06-04 17:05:11 +0500

    firstdayinwork

--- lib/main.dart
+++ lib/main.dart
@@ -29,8 +29,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-  TextEditingController loginController = TextEditingController(), passwordController = TextEditingController();
+  TextEditingController loginController = TextEditingController(),
+      passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -66,46 +66,54 @@ class _MyHomePageState extends State<MyHomePage> {
               width: 270,
               decoration: BoxDecoration(
                 color: const Color(0xFFEFEFEF),
-                borderRadius: BorderRadius.only(topLeft: Radius.circular(4),topRight: Radius.circular(4), bottomRight: Radius.circular(1),bottomLeft: Radius.circular(1)),
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(4),
+                    topRight: Radius.circular(4),
+                    bottomRight: Radius.circular(1),
+                    bottomLeft: Radius.circular(1)),
               ),
               child: TextField(
-                controller: loginController,
+                  controller: loginController,
                   decoration: InputDecoration(
                     focusedBorder: UnderlineInputBorder(
                       borderSide: BorderSide(color: Colors.blue),
                     ),
                     border: InputBorder.none,
-                errorBorder: InputBorder.none,
-                disabledBorder: InputBorder.none,
-                contentPadding:
-                    EdgeInsets.only(left: 15, bottom: 11, top: 11, right: 15),
-                hintText: "Email",
-              )),
+                    errorBorder: InputBorder.none,
+                    disabledBorder: InputBorder.none,
+                    contentPadding: EdgeInsets.only(
+                        left: 15, bottom: 11, top: 11, right: 15),
+                    hintText: "Email",
+                  )),
             ),
             Container(
               width: 270,
-              margin: EdgeInsets.only(top: 20,bottom: 20),
+              margin: EdgeInsets.only(top: 20, bottom: 20),
               decoration: BoxDecoration(
                 color: const Color(0xFFEFEFEF),
-                borderRadius: BorderRadius.only(topLeft: Radius.circular(4),topRight: Radius.circular(4), bottomRight: Radius.circular(1),bottomLeft: Radius.circular(1)),
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(4),
+                    topRight: Radius.circular(4),
+                    bottomRight: Radius.circular(1),
+                    bottomLeft: Radius.circular(1)),
               ),
               child: TextField(
-                controller: passwordController,
-                obscureText: true,
+                  controller: passwordController,
+                  obscureText: true,
                   decoration: InputDecoration(
-                border: InputBorder.none,
+                    border: InputBorder.none,
                     focusedBorder: UnderlineInputBorder(
                       borderSide: BorderSide(color: Colors.blue),
                     ),
-                errorBorder: InputBorder.none,
-                disabledBorder: InputBorder.none,
-                contentPadding:
-                    EdgeInsets.only(left: 15, bottom: 11, top: 11, right: 15),
-                hintText: "Пароль",
-              )),
+                    errorBorder: InputBorder.none,
+                    disabledBorder: InputBorder.none,
+                    contentPadding: EdgeInsets.only(
+                        left: 15, bottom: 11, top: 11, right: 15),
+                    hintText: "Пароль",
+                  )),
             ),
             Container(
-              margin: EdgeInsets.only(top:16),
+                margin: EdgeInsets.only(top: 16),
                 child: MaterialButton(
                     onPressed: () {
                       _checkInputOnNull();
@@ -126,10 +134,10 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  void _checkInputOnNull(){
+  void _checkInputOnNull() {
     String login = loginController.text;
     String password = passwordController.text;
-    if(login == '' || password == ''){
+    if (login == '' || password == '') {
       Fluttertoast.showToast(
           msg: "Заполните все поля",
           toastLength: Toast.LENGTH_SHORT,
@@ -137,9 +145,7 @@ class _MyHomePageState extends State<MyHomePage> {
           timeInSecForIosWeb: 1,
           backgroundColor: Colors.red,
           textColor: Colors.white,
-          fontSize: 16.0
-      );
+          fontSize: 16.0);
     }
-
   }
 }
